688bcd0f86825c8b48a18e960fcb3e0cfcd2729e
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-07-28 12:08:43 +0100

    created logic for deleting todos

--- src/components/TodoDisplayBox/TodoDisplayBox.jsx
+++ src/components/TodoDisplayBox/TodoDisplayBox.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 import "./TodoDisplayBox.scss";
 
-const TodoDisplayBox = ({ text }) => {
-  return <div>{text}</div>;
+const TodoDisplayBox = ({ text, handleSubmit }) => {
+  const [checked, setChecked] = useState(false);
+  return (
+    <form className="todo-display-box" onSubmit={handleSubmit}>
+      <input
+        className="todo-display-box__input"
+        type="checkbox"
+        name={text}
+        id={text}
+      />
+      <label className="todo-display-box__label" htmlFor={text}>
+        {text}
+      </label>
+      <button className="todo-display-box__button" type="submit" value="submit">
+        -
+      </button>
+    </form>
+  );
 };
 
 export default TodoDisplayBox;
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import TodoInputBox from "../../components/TodoInputBox/TodoInputBox";
-import TodoDisplayBox from "../../components/TodoInputBox/TodoInputBox";
+import TodoDisplayBox from "../../components/TodoDisplayBox/TodoDisplayBox";
 import "./Main.scss";
 
 const Main = () => {
@@ -19,6 +19,14 @@ const Main = () => {
     e.target[0].value = "";
   };
 
+  const handleRemoveTodo = (e) => {
+    e.preventDefault();
+    const indexToRemove = todos.indexOf(e.target[0].name);
+    const updatedTodos = [...todos];
+    updatedTodos.splice(indexToRemove, 1);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="main">
       <TodoInputBox handleSubmit={handleAddTodo} />
@@ -27,7 +35,9 @@ const Main = () => {
           Nothing to see here yet... Add a task in the field above!
         </p>
       ) : (
-        todos.map((todo, i) => <TodoDisplayBox key={i} text={todo} />)
+        todos.map((todo, i) => (
+          <TodoDisplayBox key={i} text={todo} handleSubmit={handleRemoveTodo} />
+        ))
       )}
     </div>
   );
