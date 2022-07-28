6fdfced959214944d4570683a3c092e72186eae4
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-07-28 13:38:46 +0100

    added a function to prevent repeating the same todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,3 @@
-// import { useState } from "react";
-
-// import Header from "./containers/Header/Header";
-// import Main from "./containers/Main/Main";
-// import "./App.scss";
-
-// const App = () => {
-//   const [todos, setTodos] = useState([]);
-
-//   const handleReset = () => {
-//     setTodos([]);
-//   };
-
-//   const handleAddTodo = (e) => {
-//     e.preventDefault();
-//     const todoItem = e.target[0].value;
-//     if (todoItem) {
-//       setTodos([...todos, todoItem]);
-//     } else {
-//       alert("please enter a todo");
-//     }
-//     e.target[0].value = "";
-//   };
-
-//   const handleRemoveTodo = (e) => {
-//     e.preventDefault();
-//     const indexToRemove = todos.indexOf(e.target[0].name);
-//     const updatedTodos = [...todos];
-//     updatedTodos.splice(indexToRemove, 1);
-//     setTodos(updatedTodos);
-//   };
-//   return (
-//     <div className="app">
-//       <Header handleReset={handleReset} />
-//       <Main
-//         handleAddTodo={handleAddTodo}
-//         handleRemoveTodo={handleRemoveTodo}
-//         todos={todos}
-//       />
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import { useState } from "react";
 
 import Header from "./containers/Header/Header";
@@ -58,6 +13,11 @@ const App = () => {
 
   const handleAddTodo = (e) => {
     e.preventDefault();
+    if (todos.some((todo) => todo.name === e.target[0].value)) {
+      alert("this item already exists");
+      e.target[0].value = "";
+      return;
+    }
     const todoItem = { name: e.target[0].value, crossedOut: false };
     if (todoItem.name) {
       setTodos([...todos, todoItem]);
